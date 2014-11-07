b26483098339872243f72b2e58ddca913030620e
金健翔 <andy_ios@163.com>
2014-11-07 21:42:21 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var builder = require('./lib/ipaBuilder.js');
+var builder = require('./lib/ipa-build.js');
 
 module.exports = function (xcodeproj_path, ipa_path, build_path ,project_name, app_path) {
 
