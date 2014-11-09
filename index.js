51961dce29c30dd8325b0ae3ee4fa5265e093975
alfred sang <i5ting@126.com>
2014-11-09 09:42:29 +0800

    Update index.js
    无意义空行

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var builder = require('./lib/ipa-build.js');
 
 module.exports = function (xcodeproj_path, ipa_path, build_path ,project_name, app_path) {
-
 	builder.xcodebuild(xcodeproj_path, project_name, build_path);
 	builder.xcrun(app_path, ipa_path);
 }
